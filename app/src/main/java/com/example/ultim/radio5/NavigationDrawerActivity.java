cab40d9522d93a2b0280bfe7d586d4589167b83d
Qwerty Qwerty <Qwerty Qwerty>
2017-04-08 17:19:53 +0300

    almost done

--- app/src/main/java/com/example/ultim/radio5/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/ultim/radio5/NavigationDrawerActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,7 +14,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.ultim.radio5.Univesity.UniversityItem;
 import com.example.ultim.radio5.Univesity.UnivesityListAdapter;
@@ -24,6 +31,17 @@ public class NavigationDrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     ListView universityListView;
     UnivesityListAdapter univesityListAdapter;
+    Switch nav_menu_switch;
+    SearchView nav_menu_searchView;
+    LinearLayout nav_menu_settingsLayout;
+
+    LinearLayout onlineContent;
+    LinearLayout offlineContent;
+    TextView networkStatusTextBox;
+    RadioGroup musicGenresGroup;
+
+    ArrayList<String> musicGengres;  //жанры музыки
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,10 +74,42 @@ public class NavigationDrawerActivity extends AppCompatActivity
         universityItems.add(new UniversityItem("МарГУ"));
         universityItems.add(new UniversityItem("МОСИ"));
 
+        onlineContent = (LinearLayout) findViewById(R.id.online_content);
+        offlineContent = (LinearLayout) findViewById(R.id.offline_content);
+        networkStatusTextBox = (TextView) findViewById(R.id.menu_network_status_item);
+
         universityListView = (ListView) findViewById(R.id.menu_university_list_view);
         univesityListAdapter = new UnivesityListAdapter(universityItems, this);
         universityListView.setAdapter(univesityListAdapter);
 
+        nav_menu_switch = (Switch) findViewById(R.id.nav_menu_switch);
+        nav_menu_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(!b) {
+                    onlineContent.setVisibility(View.VISIBLE);
+                    offlineContent.setVisibility(View.GONE);
+                    networkStatusTextBox.setText("Офлайн");
+                }
+                else {
+                    onlineContent.setVisibility(View.GONE);
+                    offlineContent.setVisibility(View.VISIBLE);
+                    networkStatusTextBox.setText("Онлайн");
+                }
+            }
+        });
+
+        musicGengres = new ArrayList<>();
+        musicGengres.add("Rock");
+        musicGengres.add("Pop");
+        musicGengres.add("Jazz");
+
+        musicGenresGroup = (RadioGroup) findViewById(R.id.music_genres_group);
+        for(String genre : musicGengres){
+            RadioButton b = new RadioButton(this);
+            b.setText(genre);
+            musicGenresGroup.addView(b); //the RadioButtons are added to the radioGroup instead of the layout
+        }
 
     }
 
@@ -105,5 +155,9 @@ public class NavigationDrawerActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void onSwitchChange() {
+
     }
 }
